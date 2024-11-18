a42deb9d3e74bf0ca3980df0907c8dcbb19e4995
zrq <3480197195@qq.com>
2024-11-18 17:32:59 +0800

    5

--- weapon/weapon.gd
+++ weapon/weapon.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
 @onready var shoot_pos = $shoot_pos
+@onready var enemy:CharacterBody2D = $enemy
 @onready var timer = $Timer
 @onready var bullet = preload("res://bullet/bullet.tscn")
 
 var shoot_time = 1
-var bullet_speed = 1400
+var bullet_speed = 400
 var bullet_hurt = 10
 
 # Called when the node enters the scene tree for the first time.
@@ -19,10 +20,17 @@ func _process(delta: float) -> void:
 
 
 func _on_timer_timeout() -> void:
-	var now_bullet : Bullet = bullet.instantiate()
+	print('enemy', "_on_timer_timeout")
+	var now_bullet :  = bullet.instantiate()
 	now_bullet.speed = bullet_speed
 	now_bullet.hurt = bullet_hurt
-	now_bullet.dir = Vector2(1, 0)
 	now_bullet.position = shoot_pos.global_position
+	var enemise = get_tree().get_nodes_in_group("enemy")
+	var player:CharacterBody2D = get_tree().get_nodes_in_group("player")[0]
+	for item in enemise: 
+		var enemy = item as Node2D
+		now_bullet.dir = (enemy.position - player.position - position).normalized()
+		print('enemy', enemy.position)
+		print('enemy', player.position + position)
 	get_tree().root.add_child(now_bullet)
 	pass # Replace with function body.
