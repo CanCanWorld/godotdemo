78ef4b14966620d66117f3dcfbde48d6710eedc3
zrq <3480197195@qq.com>
2024-11-27 17:36:07 +0800

    14

--- player/player.gd
+++ player/player.gd
@@ -8,11 +8,6 @@ var sprintTime = 0
 @onready var ball = $RigidBody2D
 @onready var sprintProgress = $SprintProgress
 @onready var recovery_tscn = preload("res://recovery/recovery.tscn")
-var sprint_time_sum = 2.5
-var sprint_time_sleep_sum = 2
-var sprint_time_sleep = 0
-var isCanSprint = true
-var sprint_scale = 2
 var lastDir = Vector2.ZERO
 var max_hp = 1000
 var hp = max_hp
@@ -22,6 +17,8 @@ var exp = 0
 var level = 1
 var recovery = 0
 var attack = 100
+var paths : Array[Vector2] = []
+var last_position : Vector2 = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -31,61 +28,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var y = Input.get_axis("下", "上")
-	var vector = Input.get_vector("左","右","上", "下")
-	if Input.is_action_just_pressed("冲刺"):
-		if isCanSprint:
-			sprintTime = sprint_time_sum
-	if sprintTime > 0:
-		#冲刺
-		isCanSprint = false
-		sprintTime -= delta
-		sprintProgress.value = sprintTime * 100 / sprint_time_sum
-		if vector.x > 0:
-			playerAnim.animation = "run_right"
-		elif vector.x < 0:
-			playerAnim.animation = "run_left"
-		elif vector.x == 0:
-			if vector.y > 0:
-				playerAnim.animation = "run"
-			elif vector.y < 0:
-				playerAnim.animation = "run_back"
-		position += vector * speed * sprint_scale
-		if sprintTime < 0:
-			sprint_time_sleep = sprint_time_sleep_sum
-	elif vector.x != 0||vector.y != 0:
-		#行走
-		lastDir = vector
-		playerAnim.speed_scale = 1
-		if vector.x > 0:
-			playerAnim.animation = "walk_right"
-		elif vector.x < 0:
-			playerAnim.animation = "walk_left"
-		elif vector.x == 0:
-			if vector.y > 0:
-				playerAnim.animation = "walk"
-			elif vector.y < 0:
-				playerAnim.animation = "walk_back"
-		position += vector * speed
-	if sprint_time_sleep > 0:
-		#等待冲刺冷却
-		sprint_time_sleep -= delta
-		sprintProgress.value = 100 - sprint_time_sleep / sprint_time_sleep_sum * 100
-		if sprint_time_sleep <= 0:
-			isCanSprint = true
-	if vector.x == 0&&vector.y == 0:
-		#停下
-		if lastDir.x > 0:
-			playerAnim.animation = "idle_right"
-		elif lastDir.x < 0:
-			playerAnim.animation = "idle_left"
-		elif lastDir.x == 0:
-			if lastDir.y > 0:
-				playerAnim.animation = "idle"
-			elif lastDir.y < 0:
-				playerAnim.animation = "idle_back"
+	walk()
 	move_and_slide()
 	judge_hp()
+	add_path()
 	pass
 	
 func choosePlayer(name: String):
@@ -158,3 +104,42 @@ func judge_hp():
 		var end : EndUi = get_tree().root.get_node("/root/bgMap/end")
 		end.is_success = false
 		end.show()
+
+func walk():
+	var y = Input.get_axis("下", "上")
+	var vector = Input.get_vector("左","右","上", "下")
+	if vector.x != 0||vector.y != 0:
+		#行走
+		lastDir = vector
+		playerAnim.speed_scale = 1
+		if vector.x > 0:
+			playerAnim.animation = "walk_right"
+		elif vector.x < 0:
+			playerAnim.animation = "walk_left"
+		elif vector.x == 0:
+			if vector.y > 0:
+				playerAnim.animation = "walk"
+			elif vector.y < 0:
+				playerAnim.animation = "walk_back"
+		position += vector * speed
+	if vector.x == 0&&vector.y == 0:
+		#停下
+		if lastDir.x > 0:
+			playerAnim.animation = "idle_right"
+		elif lastDir.x < 0:
+			playerAnim.animation = "idle_left"
+		elif lastDir.x == 0:
+			if lastDir.y > 0:
+				playerAnim.animation = "idle"
+			elif lastDir.y < 0:
+				playerAnim.animation = "idle_back"
+	
+func add_path():
+	if paths.size() == 0:
+		paths.append(global_position)
+	if global_position.distance_to(last_position) >= 100:
+		print("移动了200")
+		paths.append(last_position)
+		#print(paths)
+		last_position = global_position
+	pass
